3a4204640c0a56584a5deb9ec0d678ebdee813f2
Omar <omarvx211@gmail.com>
2025-04-09 14:49:51 +0200

    feat: implement chat prompt functionality with language translation

--- prompttemplate.ts
+++ prompttemplate.ts
@@ -1,4 +1,22 @@
-import { PromptTemplate } from '@langchain/core/prompts'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+
+const systemTemplate =
+  'You are a helpful assistant that translates {input_language} to {output_language}.'
+const humanTemplate = '{text}'
+
+const chatPrompt = ChatPromptTemplate.fromMessages([
+  ['system', systemTemplate],
+  ['human', humanTemplate],
+])
+
+// Format the messages
+const formattedChatPrompt = await chatPrompt.formatMessages({
+  input_language: 'English',
+  output_language: 'Arabic',
+  text: 'I love programming.',
+})
+
+console.log(formattedChatPrompt)
 
 // const template = 'Tell me a {adjective} joke about {content}.'
 
