5a1ada0937041eaccf007c221d1d346726a035ab
Omar <omarvx211@gmail.com>
2025-04-13 10:03:26 +0200

    Read LLM Integrations, Chat Model Integrations. Instantiate & call models (.invoke(), .batch(), .stream()).

--- chatModels.ts
+++ chatModels.ts
@@ -1,6 +1,7 @@
-import { JsonOutputParser } from '@langchain/core/output_parsers'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
-import { RunnableSequence } from '@langchain/core/runnables'
+import {
+  JsonOutputParser,
+  StringOutputParser,
+} from '@langchain/core/output_parsers'
 import { ChatGroq } from '@langchain/groq'
 
 // 2. Create the parser from the Zod schema
@@ -10,31 +11,49 @@ const parser = new JsonOutputParser()
 const llm = new ChatGroq({
   model: 'llama-3.3-70b-versatile',
   temperature: 0,
-  maxTokens: undefined,
-  maxRetries: 2,
-})
+}).pipe(new StringOutputParser())
 
-// 4. Create the prompt using the format instructions
-const prompt = ChatPromptTemplate.fromMessages([
-  {
-    role: 'system',
-    content: 'You are a helpful assistant.',
-  },
-  {
-    role: 'user',
-    content: `Please provide a brief technical comparison between chatbots and LLMs in JSON format. Use technical language.`,
-  },
-])
+const respnese = await llm.batch(['hi ther', 'who are you', 'what is 4 + 5'])
+console.log(respnese)
+console.log(
+  respnese.forEach((x) => {
+    console.log(x)
+  })
+)
 
-// 5. Create the chain
-const chain = RunnableSequence.from([
-  prompt,
+// const respnese = await llm.invoke('hi there tell me about you')
 
-  llm,
-  // (prevResult) => console.log(prevResult),
-  parser,
-])
+// console.log(respnese.content)
 
-// 6. Invoke the chain and log result
-const result = await chain.invoke({})
-console.log(result)
+// const stream = await llm.stream(
+//   'Write me a 1 verse song about goldfish on the moon'
+// )
+
+// for await (const chunk of stream) {
+//   console.log(`${chunk.content} ---`)
+// }
+
+// // 4. Create the prompt using the format instructions
+// const prompt = ChatPromptTemplate.fromMessages([
+//   {
+//     role: 'system',
+//     content: 'You are a helpful assistant.',
+//   },
+//   {
+//     role: 'user',
+//     content: `Please provide a brief technical comparison between chatbots and LLMs in JSON format. Use technical language.`,
+//   },
+// ])
+
+// // 5. Create the chain
+// const chain = RunnableSequence.from([
+//   prompt,
+
+//   llm,
+//   // (prevResult) => console.log(prevResult),
+//   parser,
+// ])
+
+// // 6. Invoke the chain and log result
+// const result = await chain.invoke({})
+// console.log(result)
--- prompttemplate.ts
+++ prompttemplate.ts
@@ -1,83 +1,111 @@
-import { ChatPromptTemplate, PromptTemplate } from '@langchain/core/prompts'
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from '@langchain/core/prompts'
+import { HumanMessage, SystemMessage } from '@langchain/core/messages'
+import { ChatGroq } from '@langchain/groq'
 
-const systemTemplate =
-  'You are a helpful assistant that translates {input_language} to {output_language}.'
-const humanTemplate = '{text}'
-
-const chatPrompt = ChatPromptTemplate.fromMessages([
-  ['system', systemTemplate],
-  ['human', humanTemplate],
-])
-
-// Format the messages
-const formattedChatPrompt = await chatPrompt.formatMessages({
-  input_language: 'English',
-  output_language: 'Arabic',
-  text: 'I love programming.',
+const model = new ChatGroq({
+  model: 'llama-3.3-70b-versatile',
+  temperature: 0,
+  maxTokens: undefined,
+  maxRetries: 2,
 })
 
-console.log(formattedChatPrompt)
+const messages = [
+  new SystemMessage('Translate the following from English into Arabic'),
+  new HumanMessage('hi!'),
+]
 
-const template = 'Tell me a {adjective} joke about {content}.'
+const re = await model.invoke(messages)
+console.log(re.content)
 
-const promptTemplate = PromptTemplate.fromTemplate(template)
-console.log(promptTemplate.inputVariables) // [('adjective', 'content')] // i seeee
-const formattedPromptTemplate = await promptTemplate.format({
-  adjective: 'funny',
-  content: 'chickens',
-  
-})
-console.log(formattedPromptTemplate)
+// const promptTemplate = ChatPromptTemplate.fromMessages([
+//   ['system', 'You are a helpful assistant'],
+//   new MessagesPlaceholder('msgs'),
+// ])
 
-// An example prompt with no input variables
-const noInputPrompt = new PromptTemplate({
-  inputVariables: [],
-  template: 'Tell me a joke.',
-})
-const formattedNoInputPrompt = await noInputPrompt.format({})
+// const re = await promptTemplate.invoke({ msgs: ['hi there', "im omar"] })
 
-console.log(formattedNoInputPrompt)
-;('Tell me a joke.')
+// console.log(re.messages)
 
-// An example prompt with one input variable
-const oneInputPrompt = new PromptTemplate({
-  inputVariables: ['adjective'],
-  template: 'Tell me a {adjective} joke.',
-})
-const formattedOneInputPrompt = await oneInputPrompt.format({
-  adjective: 'funny',
-})
+// const systemTemplate =
+//   'You are a helpful assistant that translates {input_language} to {output_language}.'
+// const humanTemplate = '{text}'
 
-console.log(formattedOneInputPrompt)
-;('Tell me a funny joke.')
+// const chatPrompt = ChatPromptTemplate.fromMessages([
+//   ['system', systemTemplate],
+//   ['human', humanTemplate],
+// ])
 
-// An example prompt with multiple input variables
-const multipleInputPrompt = new PromptTemplate({
-  inputVariables: ['adjective', 'content'],
-  template: 'Tell me a {adjective} joke about {content}.',
-})
-const formattedMultipleInputPrompt = await multipleInputPrompt.format({
-  adjective: 'funny',
-  content: 'chickens',
-})
+// // Format the messages
+// const formattedChatPrompt = await chatPrompt.formatMessages({
+//   input_language: 'English',
+//   output_language: 'Arabic',
+//   text: 'I love programming.',
+// })
 
-console.log(formattedMultipleInputPrompt)
-;('Tell me a funny joke about chickens.')
+// console.log(formattedChatPrompt)
 
-// If a template is passed in, the input variables are inferred automatically from the template.
-const prompt = PromptTemplate.fromTemplate(
-  `You are a naming consultant for new companies.
-  What is a good name for a company that makes {{product}}?`,
-  { templateFormat: 'mustache' } //  double  {} for the input var
-)
+// const template = 'Tell me a {adjective} joke about {content}.'
 
-const formattedPrompt = await prompt.format({
-  product: 'colorful socks',
-})
+// const promptTemplate = PromptTemplate.fromTemplate(template)
+// console.log(promptTemplate.inputVariables) // [('adjective', 'content')] // i seeee
+// const formattedPromptTemplate = await promptTemplate.format({
+//   adjective: 'funny',
+//   content: 'chickens',
+// })
+// console.log(formattedPromptTemplate)
 
-console.log(formattedPrompt)
+// // An example prompt with no input variables
+// const noInputPrompt = new PromptTemplate({
+//   inputVariables: [],
+//   template: 'Tell me a joke.',
+// })
+// const formattedNoInputPrompt = await noInputPrompt.format({})
 
-/*
-You are a naming consultant for new companies.
-What is a good name for a company that makes colorful socks?
-*/
+// console.log(formattedNoInputPrompt)
+// ;('Tell me a joke.')
+
+// // An example prompt with one input variable
+// const oneInputPrompt = new PromptTemplate({
+//   inputVariables: ['adjective'],
+//   template: 'Tell me a {adjective} joke.',
+// })
+// const formattedOneInputPrompt = await oneInputPrompt.format({
+//   adjective: 'funny',
+// })
+
+// console.log(formattedOneInputPrompt)
+// ;('Tell me a funny joke.')
+
+// // An example prompt with multiple input variables
+// const multipleInputPrompt = new PromptTemplate({
+//   inputVariables: ['adjective', 'content'],
+//   template: 'Tell me a {adjective} joke about {content}.',
+// })
+// const formattedMultipleInputPrompt = await multipleInputPrompt.format({
+//   adjective: 'funny',
+//   content: 'chickens',
+// })
+
+// console.log(formattedMultipleInputPrompt)
+// ;('Tell me a funny joke about chickens.')
+
+// // If a template is passed in, the input variables are inferred automatically from the template.
+// const prompt = PromptTemplate.fromTemplate(
+//   `You are a naming consultant for new companies.
+//   What is a good name for a company that makes {{product}}?`,
+//   { templateFormat: 'mustache' } //  double  {} for the input var
+// )
+
+// const formattedPrompt = await prompt.format({
+//   product: 'colorful socks',
+// })
+
+// console.log(formattedPrompt)
+
+// /*
+// You are a naming consultant for new companies.
+// What is a good name for a company that makes colorful socks?
+// */
