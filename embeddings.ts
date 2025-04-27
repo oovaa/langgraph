24f9fd0b9418382d2ec7e24220f65a3a739ad46c
Omar <omarvx211@gmail.com>
2025-04-27 16:04:25 +0200

    Refactor embeddings.ts to integrate MemoryVectorStore and add document handling for similarity search

--- embeddings.ts
+++ embeddings.ts
@@ -1,12 +1,36 @@
 import { CohereEmbeddings } from '@langchain/cohere'
-const embeddingsModel = new CohereEmbeddings({ model: 'embed-v4.0' })
-const embeddings = await embeddingsModel.embedDocuments([
-  'Hi there!',
-  'Oh, hello!',
-  "What's your name?",
-  'My friends call me World',
-  'Hello World!',
-])
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+import type { Document } from '@langchain/core/documents'
 
-console.log(`(${embeddings.length}, ${embeddings[0].length})`)
-// (5, 1536)
+const embeddingsModel = new CohereEmbeddings({ model: 'embed-v4.0' })
+
+const vectorStore = new MemoryVectorStore(embeddingsModel)
+
+const document1: Document = {
+  pageContent: 'The powerhouse of the cell is the mitochondria',
+  metadata: { source: 'https://example.com' },
+}
+
+const document2: Document = {
+  pageContent: 'Buildings are made out of brick',
+  metadata: { source: 'https://example.com' },
+}
+
+const document3: Document = {
+  pageContent: 'Mitochondria are made out of lipids',
+  metadata: { source: 'https://example.com' },
+}
+
+const documents = [document1, document2, document3]
+
+await vectorStore.addDocuments(documents)
+
+const similaritySearchResults = await vectorStore.similaritySearchWithScore(
+  'construction'
+)
+
+const re = similaritySearchResults.filter((x) => x[1] > 0.2)
+
+for (const doc of re) {
+  console.log(`* ${doc[0].pageContent} [${JSON.stringify(doc[1], null)}]`)
+}
